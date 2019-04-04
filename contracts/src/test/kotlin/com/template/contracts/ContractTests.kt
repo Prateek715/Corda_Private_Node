26ede8ddce3629bfda77a9e03aee687f4e7d22d5
prateektiwari7 <prateek.tiwari@yudiz.com>
2019-04-04 15:40:53 +0530

    ADD

--- contracts/src/test/kotlin/com/template/contracts/ContractTests.kt
+++ contracts/src/test/kotlin/com/template/contracts/ContractTests.kt
@@ -10,4 +10,7 @@ class ContractTests {
     fun `dummy test`() {
 
     }
+
+
+
 }
---
+++ workflows/src/integrationTest/kotlin/com/template/CreateNode.kt
@@ -0,0 +1,104 @@
+package com.template
+import com.template.Permissions.Companion.invokeRpc
+import net.corda.testing.driver.DriverParameters
+import net.corda.testing.driver.driver
+import net.corda.testing.node.internal.FINANCE_CORDAPPS
+import org.h2.engine.User
+import org.junit.Test
+import com.template.Permissions.Companion.startFlow
+import net.corda.finance.flows.CashIssueAndPaymentFlow
+import com.template.ALICE_NAME
+import net.corda.client.rpc.CordaRPCClient
+import net.corda.core.messaging.CordaRPCOps
+import net.corda.core.messaging.vaultTrackBy
+import net.corda.core.node.services.Vault
+import net.corda.core.utilities.getOrThrow
+import net.corda.finance.contracts.asset.Cash
+import rx.Observable
+import java.util.*
+
+class CreateNode {
+
+    @Test
+    fun aliceandbobexchange(){
+
+       driver(DriverParameters(startNodesInProcess = true,cordappsForAllNodes = FINANCE_CORDAPPS)){
+           val prateek= net.corda.testing.node.User("prateek","testpassword1",permissions = setOf(
+            startFlow<CashIssueAndPaymentFlow>(),
+                   invokeRpc("vaultTrackBy")
+           ))
+
+
+
+           val Prateektest= net.corda.testing.node.User("prateekTest","testpassord2",permissions = setOf(
+                   startFlow<CashIssueAndPaymentFlow>(),
+                   invokeRpc("vaultTrackBy")
+           ))
+
+
+           val(alice,bob) = listOf(
+                   startNode(providedName = ALICE_NAME,rpcUsers = listOf(prateek)),
+                    startNode(providedName = BOB_NAME,rpcUsers = listOf(Prateektest))
+           ).map { it.getOrThrow() }
+
+
+           val  aliceClient = CordaRPCClient(alice.rpcAddress)
+
+          println("AliceClient"+"##############################"+aliceClient);
+
+           val aliceProxy : CordaRPCOps = aliceClient.start("prateek","testpassword1").proxy;
+
+           println("AliceProxy"+"################################"+aliceProxy);
+
+
+           val BobCkient = CordaRPCClient(bob.rpcAddress)
+
+           val bobProxy :CordaRPCOps = BobCkient.start("prateekTest","testpassord2").proxy;
+
+           println("@@@@@@@@@@@@@@@@@@@"+bobProxy);
+
+
+           val bobVaultUpdates: Observable<Vault.Update<Cash.State>> = bobProxy.vaultTrackBy<Cash.State>().updates
+
+            val aliceUpdates: Observable<Vault.Update<Cash.State>> = aliceProxy.vaultTrackBy<Cash.State>().updates
+
+
+           println("%%%%%%%%%%%%%%%%%%%%"+bobVaultUpdates)
+
+           println("^^^^^^^^^^^^^^^^^^^^"+aliceUpdates)
+
+
+
+
+
+
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
